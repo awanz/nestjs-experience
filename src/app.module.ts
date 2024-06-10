fbd5033dff175af8c2476be92f0f4e2aed253196
Awan <me@awn.web.id>
2024-06-10 09:41:20 +0700

    add package typeorm, mssql and setup connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mssql',
+      host: 'localhost',
+      port: 1433,
+      username: 'SA',
+      password: 'AwanSky123!',
+      database: 'nest',
+      extra: {
+        trustServerCertificate: true,
+      },
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      logging: true,
+      synchronize: false,
+      migrationsTableName: 'typeorm_migrations',
+      migrationsRun: false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
